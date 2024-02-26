acba1355a77a6d62d3ad4f49917ac176aba8a338
rosie <rosie@MacBook-Pro-77.local>
2024-02-26 12:50:45 -0700

    updated files

---
+++ files/apostrophe.py
@@ -0,0 +1,5 @@
+##message='One of Python's strengths is its diverse community.'
+#print(message) #python can't run the message becayse of the muyltiple single quotes, if I use double quotes it can run it
+
+message="One of Python's strengths is its diverse community."
+print(message)
