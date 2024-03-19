d2cb2cfa86a8d1808420a349e9ca6f19cb07a68f
rosie <rosie@MacBook-Pro-77.local>
2024-03-19 09:24:07 -0600

    files update

---
+++ files/python_for_data scince.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+hour=pd.read_csv('hour.csv')
+print(hour.head())
+
+print(hour['count'].mean())
+print(hour['count'].median())
+print(hour['count'].std())
+print(hour['registered'].min())
+print(hour['registered'].max())
+
+print(hour.describe()) #a quick way to get summary statistics
+
+
+
